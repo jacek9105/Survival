46d0ddeb2de616d9b391d8713d639cca44e99206
spider87pl <grzspe@st.amu.edu.pl>
2019-11-12 21:44:14 +0100

    ekwipunek 1
    baza danych pod ekwipunek, 1 przedmiot - siekiera

---
+++ Assets/Survival3D/scripts/equipment/Database.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Database : MonoBehaviour
+{
+    public List<Object> itemList = new List<Object> ();
+
+
+    void Awake()
+    {
+        itemList.Add(new Object(0, "Siekiera", "Siekiera bez krwi"));
+
+
+    }
+}
