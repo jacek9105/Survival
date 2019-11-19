7739d55dfb03d0416fc0f44f01ba47ca5275b791
mross-wmi <matros1@st.amu.edu.pl>
2019-11-19 20:31:43 +0100

    Update metadanych
    git mnie zmusza, nic nie zmieniałem

---
+++ Assets/Survival3D/scripts/Axe/Axe.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    public Animation anim;
+    public bool hitAble;
+    public bool hited;
+
+    void Start()
+    {
+        hitAble = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && hitAble == true)
+        {
+            anim.Play();
+        }
+
+        if (anim.isPlaying == false)
+        {
+            hitAble = true;
+            hited = false;
+        }
+        else
+        {
+            hitAble = false;
+            hited = true;
+        }
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Tree" && hited == true)
+        {
+            collider.gameObject.GetComponent<Tree_Felling>().TreeHP -= 1;
+        }
+    }
+
+}
