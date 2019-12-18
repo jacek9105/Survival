3709b0eb24b831944bc6d16d1fcc594fbf933f36
mross-wmi <matros1@st.amu.edu.pl>
2019-12-18 16:35:09 +0100

    Weapon switch
    primary = key 1 (knife)
    secondary = key 2 (axe)
    melee = key 3

--- Assets/Survival3D/scripts/WeaponsSwitch/WeaponSwitch.cs
+++ Assets/Survival3D/scripts/WeaponsSwitch/WeaponSwitch.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class WeaponSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int weaponSelected = 1;
+
+    [SerializeField]
+    GameObject primary, secondary, melee;
+
     void Start()
     {
-        
+        SwapWeapon(1);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if(weaponSelected != 1)
+            {
+                SwapWeapon(1);              
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (weaponSelected != 2)
+            {
+                SwapWeapon(2);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (weaponSelected != 3)
+            {
+                SwapWeapon(3);
+            }
+        }
+    }
+
+    void SwapWeapon(int weaponType)
+    {
+        if(weaponType == 1)
+        {
+            primary.SetActive(true);
+            secondary.SetActive(false);
+            melee.SetActive(false);
+
+            weaponSelected = 1;
+        }
+
+        if (weaponType == 2)
+        {
+            primary.SetActive(false);
+            secondary.SetActive(true);
+            melee.SetActive(false);
+
+            weaponSelected = 2;
+        }
+
+        if (weaponType == 3)
+        {
+            primary.SetActive(false);
+            secondary.SetActive(false);
+            melee.SetActive(true);
+
+            weaponSelected = 3;
+        }
     }
 }
