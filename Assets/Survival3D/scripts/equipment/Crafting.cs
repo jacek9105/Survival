9366a0310379382d1a4db6d6e10ff6850d06f2f7
jacek9105 <jacek9105@users.noreply.github.com>
2019-12-19 15:43:46 +0100

    crafting vol. 1

---
+++ Assets/Survival3D/scripts/equipment/Crafting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting : MonoBehaviour
+{
+    public equipment equipment;
+    public GUISkin skin;
+    int numberSocketsX;
+    int numberSocketsY;
+    bool didViewCrafting;
+
+    void Start()
+    {
+        numberSocketsX = 5;
+        numberSocketsY = 4;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            didViewCrafting = !didViewCrafting;
+        }
+    }
+    private void OnGUI()
+    {
+        if (didViewCrafting == true && equipment.didViewInventory == false)
+        {
+            viewCrafiting();
+        }
+    }
+    void viewCrafiting()
+    {
+        int i = 0;
+
+        for (int x = 0; x < numberSocketsX; x++)
+        {
+            for (int y = 0; y < numberSocketsY; y++)
+            {
+                Rect slotLocation = new Rect(Screen.width * 0.05f + (x * Screen.width * 0.075f), Screen.height * 0.05f + (y * Screen.height * 0.13f), Screen.width * 0.07f, Screen.height * 0.13f);
+                GUI.Box(slotLocation, "", skin.GetStyle("slotCraftingu"));
+            }
+        }
+    }
+}
+ 
