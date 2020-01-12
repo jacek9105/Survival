f09789316454fe33bb6298a602f3e053becb6920
jacek9105 <jacek9105@users.noreply.github.com>
2020-01-12 11:00:31 +0100

    tree

--- Assets/Survival3D/scripts/Axe/Axe.cs
+++ Assets/Survival3D/scripts/Axe/Axe.cs
@@ -4,55 +4,57 @@ using UnityEngine;
 
 public class Axe : MonoBehaviour
 {
-    public Animation anim;
+    // public Animation anim;
     public bool hitAble;
     public bool hited;
-    public AudioSource chopSound;
+    //public AudioSource chopSound;
     public equipment eq;
 
     void Start()
     {
         hitAble = true;
-        
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0) && hitAble == true)
-        {
-            anim.Play();
-        }
+    /* void Update()
+     {
+         if(Input.GetMouseButtonDown(0) && hitAble == true)
+         {
+             anim.Play();
+         }
 
-        if (anim.isPlaying == false)
-        {
-            hitAble = true;
-            hited = false;
-        }
-        else
-        {
-            hitAble = false;
-            hited = true;
-        }
-    }
+         if (anim.isPlaying == false)
+         {
+             hitAble = true;
+             hited = false;
+         }
+         else
+         {
+             hitAble = false;
+             hited = true;
+         }
+     }*/
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "Tree" && hited == true)
         {
-            if(collider.gameObject.GetComponent<Tree_Felling>().TreeHP >0)
-            { 
-            collider.gameObject.GetComponent<Tree_Felling>().TreeHP -= 1;
-                chopSound.Play();
-            }
-            if(collider.gameObject.GetComponent<Tree_Felling>().TreeHP == 0)
+            if (collider.gameObject.GetComponent<Tree_Felling>().TreeHP > -2)
             {
-                for(int i = 0; i<eq.listOwnedItem.Count; i++)
+                collider.gameObject.GetComponent<Tree_Felling>().TreeHP -= 1;
+                // chopSound.Play();
+            }
+            if (collider.gameObject.GetComponent<Tree_Felling>().TreeHP <= 0)
+            {
+                for (int i = 0; i < eq.listOwnedItem.Count; i++)
                 {
-                    if(eq.listOwnedItem[i].id == 0)
+                    if (eq.listOwnedItem[i].id == 0)
                     {
                         eq.listOwnedItem[i] = Database.itemList[2];
                         break;
                     }
+
+                 
                 }
             }
         }
