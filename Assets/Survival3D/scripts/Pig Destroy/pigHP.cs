4e2cdb9d883da7733631090d09a8569741dbfff8
jacek9105 <jacek9105@users.noreply.github.com>
2020-01-14 23:12:38 +0100

    pig dropped trunks not meal

--- Assets/Survival3D/scripts/Pig Destroy/pigHP.cs
+++ Assets/Survival3D/scripts/Pig Destroy/pigHP.cs
@@ -7,7 +7,7 @@ public class pigHP : MonoBehaviour
 
     public int pigHealtPoints;
    
-    public Transform trunk; 
+    public Transform meal; 
     public GameObject pig;
     void Start()
     {
@@ -36,9 +36,9 @@ public class pigHP : MonoBehaviour
         Destroy(gameObject);
                 
         Vector3 position = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
-        Instantiate(trunk, trunk.transform.position + new Vector3(0, 0, 0) + position, Quaternion.identity);
-        Instantiate(trunk, trunk.transform.position + new Vector3(2, 0, 0) + position, Quaternion.identity);
-        Instantiate(trunk, trunk.transform.position + new Vector3(4, 0, 0) + position, Quaternion.identity);
+        Instantiate(meal, pig.transform.position + new Vector3(0, 0, 0) + position, Quaternion.identity);
+        Instantiate(meal, pig.transform.position + new Vector3(2, 0, 0) + position, Quaternion.identity);
+       
 
 
     }
