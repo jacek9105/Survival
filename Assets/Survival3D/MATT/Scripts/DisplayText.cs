443888141851c611bfea9672636ed079f388aed0
mross-wmi <matros1@st.amu.edu.pl>
2020-01-14 20:40:28 +0100

    Text display trigger
    wyświetlanie wskazówek podczas odkrywania mapy

---
+++ Assets/Survival3D/MATT/Scripts/DisplayText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour {
+    public GameObject UIObject;
+    public GameObject Cube;
+ 
+    void Start()
+    {
+        UIObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            UIObject.SetActive(true);
+        }
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        UIObject.SetActive(false);
+        //Destroy(Cube);
+    }
+}
