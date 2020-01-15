6d3f78e09fcf2ef788fa910b0b79ec8e3de00760
jacek9105 <jacek9105@users.noreply.github.com>
2020-01-15 22:43:19 +0100

    need to pull Daniels commit

---
+++ Assets/Survival3D/scripts/Axe/PigAxe.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigAxe : MonoBehaviour
+{
+    //  public Animation anim;
+    public bool hitAble;
+    public bool hited;
+    //public AudioSource chopSound;
+    public equipment eq;
+
+    void Start()
+    {
+        hitAble = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+
+        if (Input.GetMouseButtonDown(0) && hitAble == true)
+         {
+             anim.Play();
+         }
+
+         if (anim.isPlaying == false)
+         {
+             hitAble = true;
+             hited = false;
+         }
+         else
+         {
+             hitAble = false;
+             hited = true;
+         }
+         */
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.tag == "Enemy")
+        {
+            if (collider.gameObject.GetComponent<pigHP>().pigHealtPoints >= 0)
+            {
+                collider.gameObject.GetComponent<pigHP>().pigHealtPoints -= 1;
+            }
+        }
+    }
+
+}
